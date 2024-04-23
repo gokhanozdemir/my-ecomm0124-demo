353631a5a0e12afb061292c7199fa068e4fcf4ed
Gökhan <gn.ozdemir@gmail.com>
2024-04-23 11:46:40 +0300

    Boncuk buldum Fixes #3

--- src/App.jsx
+++ src/App.jsx
@@ -21,8 +21,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        Şu an branchteyim #2
-        Issue 1 finito
+        What does this 🧿 emoji mean?
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
