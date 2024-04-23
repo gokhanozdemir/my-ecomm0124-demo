9125cd61eaaad76fff5f7285de9beb84cde62ece
Gökhan <gn.ozdemir@gmail.com>
2024-04-23 11:28:16 +0300

    gitflow anlatımı adım 1

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        selam
+        Şu an branchteyim #1
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
